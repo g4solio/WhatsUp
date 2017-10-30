7318cc2357791d8de3a07fae422587dbc9dd5cb5
GOZZI DAVIDE <d.gozzi@VINCI.IISCORNI.IT>
2017-10-30 09:46:32 +0100

    initializated project

---
+++ WhatsUpServer/src/whatsupserver/WhatsUpServer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whatsupserver;
+
+/**
+ *
+ * @author d.gozzi
+ */
+public class WhatsUpServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
