88504a9dfbcb0b2b06e65da7479e67719eed015c
g4solio <daddidaffi@gmail.com>
2017-10-31 14:02:50 +0100

    Working on Server

---
+++ WhatsUpServer/src/whatsupserver/ConnectedClient.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whatsupserver;
+
+/**
+ *
+ * @author davide
+ */
+public class ConnectedClient extends Thread
+{
+    
+    public String ip;
+    public String nickname;
+    public Room roomConnected;
+    
+    
+    @Override
+    public void run()
+    {
+       
+    }
+    
+}
